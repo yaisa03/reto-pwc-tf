42128123bf1bf7e3dcaf615b71bcfc84a993e811
Megan Palacios <meg2086@outlook.com>
2022-08-25 08:49:03 -0500

    displaying requirements on user's dashboard (not filtered yet)

--- src/views/DashboardUser.vue
+++ src/views/DashboardUser.vue
@@ -1,20 +1,34 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { projectColRef } from '../firebase'
+import { ref, onMounted } from 'vue'
+import { projectColRef, refCol } from '../firebase'
 import { onSnapshot } from '@firebase/firestore'
 const projects = ref([])
-const loading = ref(true)
+let currentProject = ref('')
+const selectId = () => {
+    window.localStorage.setItem('ID', currentProject.value)
+    console.log(window.localStorage.getItem('ID'))
+}
+
+let requirements = ref([])
+
 function getProjects() {
     onSnapshot(projectColRef, (snapshot) => {
-        const allProjects = snapshot.docs.map(doc => ({
+        const allProjects = snapshot.docs.map((doc) => ({
             data: doc.data(),
-            id: doc.id
+            id: doc.id,
         }))
         projects.value = allProjects
-        loading.value = false
+    })
+    onSnapshot(refCol, (snapshot) => {
+        const allRequirements = snapshot.docs.map((doc) => ({
+            data: doc.data(),
+            id: doc.id,
+        }))
+        requirements.value = allRequirements
     })
 }
-onMounted(getProjects);
+
+onMounted(getProjects)
 </script>
 <template>
     <div class="row g-3 align-items-center mt-1 w-50 p-3">
@@ -22,10 +36,8 @@ onMounted(getProjects);
             <label class="col-form-label fw-bold">Nombre del Proyecto</label>
         </div>
         <div class="col-auto">
-            <select class='form-select' name="project">
-                <option value="Reporte 20">Reporte 20</option>
-                <option value="Reporte 21">Reporte 21</option>
-                <option value="Reporte 22">Reporte 22</option>
+            <select v-model="currentProject" @change="selectId" class="form-select" aria-label="Default select example">
+                <option v-for="p in projects" :value="p.id">{{ p.data.name }}</option>
             </select>
         </div>
     </div>
@@ -35,29 +47,25 @@ onMounted(getProjects);
             <table class="table table-responsive align-middle ">
                 <thead class="align-middle">
                     <tr class="bg-light text-center">
-                        <th scope="col" width="15%">Nombre de proyecto</th>
-                        <th scope="col" width="10%">Tema</th>
                         <th scope="col" width="15%">Requerimiento</th>
+                        <th scope="col" width="10%">Tema</th>
+                        <th scope="col" width="10%">Pilar</th>
                         <th scope="col" width="10%">Fecha de creación</th>
                         <th scope="col" width="10%">Fecha de vencimiento</th>
-                        <th scope="col" width="10%">Fecha de aceptación</th>
                         <th scope="col" width="15%">Responsable de requerimiento</th>
                         <th scope="col" width="15%">Estatus</th>
                     </tr>
                 </thead>
                 <tbody class="text-center">
-                    <tr v-for="p in projects">
-                        <td class="text-start">
-                            <input class="form-check-input ms-4" type="checkbox"> {{ p.data.name }}
-                        </td>
-                        <td>Agua</td>
-                        <td>Consumo de agua</td>
-                        <td>1 Oct, 21</td>
-                        <td>12 Oct, 21</td>
-                        <td>12 Oct, 21</td>
-                        <td>Alvaro Olea</td>
-                        <td><i class="bi bi-circle-fill" style="font-size: 1.5rem; color: #519;"></i><span class="ms-1">
-                                En proceso</span></td>
+                    <tr v-for="r in requirements">
+                        <td>{{ r.data.requirement }}</td>
+                        <td>{{ r.data.theme }}</td>
+                        <td>{{ r.data.pillar }}</td>
+                        <td>{{ r.data.date }}</td>
+                        <td>{{ r.data.expirationDate }}</td>
+                        <td>{{ r.data.responsable }}</td>
+                        <td><i class="bi bi-circle-fill" style="font-size: 1.5rem; color: #519;"></i><span
+                                class="ms-1">{{ r.data.status }}</span></td>
                     </tr>
                 </tbody>
             </table>
